36f1dcaa0246564b3373cd4368bbb4070c2df33f
J.Liu <30520114+TL10100@users.noreply.github.com>
2017-08-28 19:57:30 +0800

    object
    object

--- Build a Quiz Challenge, Part 2/js/quiz.js
+++ Build a Quiz Challenge, Part 2/js/quiz.js
@@ -8,12 +8,22 @@ var question;
 var answer;
 var response;
 var html;
+var correct = [];
+var wrong = [];
 
 function print(message) {
-  var outputDiv = getElementById('output');
+  var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i += 1) {
+      listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;
+}
 for (var i = 0; i < questions.length; i += 1) {
   question = questions[i][0];
   answer = questions[i][1];
@@ -21,8 +31,15 @@ for (var i = 0; i < questions.length; i += 1) {
   response = parseInt(response);
   if (response === answer) {
     correctAnswers += 1;
-  } 
+    correct.push(question);
+  } else {
+    wrong.push(question);
+  }
 }
 
-html = "You got " + correctAnswers + " question(s) right."
+html = "You got " + correctAnswers + " question(s) right.";
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
 print(html);
---
+++ The Object Literal/js/object.js
@@ -0,0 +1,7 @@
+var person = {
+    name : 'Liu Jingwei',
+    contry : 'China',
+    age : 28,
+    skills : ['JavaScript','HTML','CSS'],
+    treeHouseStudents : true
+};
